032b2da4ccc8563f0d015cbbb13aab1f42ff2b31
devill_x <devill_x@epitech.eu>
2014-05-31 02:10:21 +0200

    test display of string test with macro MSG()

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,31 @@
 // Login   <vaur@epitech.net>
 //
 // Started on  Fri May 23 23:01:22 2014 vaur
-// Last update Mon May 26 16:41:57 2014 vaur
+// Last update Fri May 30 17:47:08 2014 vaur
 //
 
 /*
 ** Include
 */
 
+#include	<string>
 #include	"MsgBox.hpp"
 
 /*
 ** Functions
 */
 
-int		main(int argc, char **argv)
+int			main(int argc, char **argv)
 {
-  MsgBox	msgbox(argv[0]);
+  MsgBox		msgbox(argv[0]);
+  // DecorateBracket	decorate;
 
-  msgbox << "test";
+  std::string	test;
+
+  test = "test";
+
+  // INFO(test);
+  // msgbox.decorateIn() << test; msgbox.decorateOut();
+  MSG(test);
   return (0);
 }
